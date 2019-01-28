94674555152ac1e17c81207deafa2d18e26b6ddf
A Shrodinger <a.j.rasure@gmail.com>
2019-01-28 09:04:05 -0500

    no message

---
+++ temp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 25 12:06:09 2019
+
+@author: AJRasure
+"""
+
+import numpy as np
+
+
+x = np.array([3,4,5])
+
+x += 3
+
+print(x)
+g
