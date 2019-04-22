0bbd61de4a916e9b664f24a56ce26feb868c006d
A Shrodinger <a.j.rasure@gmail.com>
2019-04-22 14:55:08 -0400

    no message

--- Applied/A3_Deflation_Rasure.py
+++ Applied/A3_Deflation_Rasure.py
@@ -1,90 +1,68 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 17 12:44:57 2019
 
-@author: AJRasure
-"""
+def Coef(r,gx):
+    coRet =[]
+    for ii in range(len(r)-len(gx)+1):
+        coRet.append(0)
+    coRet[-1] = r[-1]/gx[-1]    
+    return coRet
 
-
-""" Division Algorithm
-
-Input: fx,gx
-Output: quotient and remeinder (q,r)
-
-q = 0, r = fx
-
-(LT = leading term)
-while r !=0 and LT(g) divides LT(f) do
-    q = q + LT(r)/LT(gx)
-    r = r - (LT(r)/LT(gx))*gx
-"""
-#coefficient from LT(fx)/LT(gx)
-def coef(r,gx):
-    return r[-1]/gx[-1]
-
-
-fx = [3,-1,0,0,0,-5,0,5] #polynomial to be divided (eg. [1,2,3] = 1+2x+3x^2)
-gx = [0,1,0,0,3]     #dividing polynomial #polynomial to be divided (eg. [0,2] = 2x)
-#creates quotient list based on fx
-
-q=[]
-for ii in range(len(fx)):
-    q.append(0)
-
-r = fx
-  
-degree = len(r)-len(gx) #exponent of LT(fx)/LT(gx)
+def PolyMult(outer,inner):
+    nx = []
+    for ii in range(len(inner)+len(outer)-1):
+        nx.append(0)
+    for ii in range(1,len(inner)+1):
+        nx[-ii] = outer[-1]*inner[-ii]
+    return nx
  
-while degree>0:
-#for jj in range(1):
-    
-    # Where the next r = r - LT(fx)/LT(gx)*gx r2 = LT(fx)/LT(gx)*gx r2
-    #establishes r2
-    r2 = []   
-    for ii in range(len(r)): #degree of r2
-        r2.append(0)
-            
-    for ii in range(-1,-len(gx),-1): #coefficients of r2   
-        r2[ii] = coef(r,gx)*gx[ii]
-    #        
-    q[degree] = q[degree] + coef(r,gx) #places the proper coefficient into the appropriate degreed space in the list
-    
-    #creates the next r where r(next) = r(current)-r2
-    for ii in range(-1,-len(r2)-1,-1):
-        r[ii] = r[ii] -r2[ii]        
-    del r[-1] #removes final 0 from list, 0x^n that will equal zero
-    
-    degree = len(r)-len(gx)
+def PolySub(fx,gx):
+    nx = []
+    for ii in range(len(fx)):
+        nx.append(0)
+    for ii in range(len(gx)):
+        nx[ii] = fx[ii]-gx[ii]
+    return nx
 
-if degree == 0:  
-
-    r2 = []
-    for ii in range(degree+1):
-        r2 = list(gx)
-    for ii in range(0,len(r2)):
-        r2[ii] = coef(r,gx)*r2[ii]
-    q[degree] = q[degree] + coef(r,gx)
-    for ii in range(-1,-len(r2)-1,-1):
-        r[ii] = r[ii] - r2[ii]
+f_x = [5,-11,-7,4] #polynomial to be divided (eg. [1,2,3] = 1+2x+3x^2)
+g_x = [5,4]     #dividing polynomial #polynomial to be divided (eg. [0,2] = 2x)
+#creates quotient list based on fx
+q=[]
+r = list(f_x)
+for ii in range(len(f_x)):
+    q.append(0)
+degree = len(r)-len(g_x)
+while degree > -1:
+    r2 = PolyMult(Coef(list(r),list(g_x)),list(g_x))
+    q[degree] = q[degree] + Coef(list(r),list(g_x))[-1]
+    r = PolySub(r,r2)
+    
     del r[-1]
+    degree = len(r)-len(g_x)
 
- # Where the next r = r - LT(fx)/LT(gx)*gx r2 = LT(fx)/LT(gx)*gx r2
-    #establishes r2
-#r2 = []   
-#for ii in range(degree+1): #degree of r2
-#    r2.append(0)
-#    
-#for ii in range(-1,-len(gx),-1): #coefficients of r2   
-#    r2[ii] = coef(r,gx)*gx[ii]
-#    
-#q[degree] = q[degree] + coef(r,gx) #places the proper coefficient into the appropriate degreed space in the list
-#
-##creates the next r where r(next) = r(current)-r2
-#for ii in range(-1,-len(r2)-1,-1):
-#    r[ii] = r[ii] -r2[ii]        
-#del r[-1] #removes final 0 from list, 0x^n that will equal zero
-#
-#degree = len(r)-len(gx)
-print(q,r)
-
+print("Through polynomial division:")   
+for ii in range(len(f_x)):
+    if ii == 0:      
+        print("({})".format(f_x[ii]), end="")    
+    elif f_x[ii] != 0:
+        print("+({})x^{}".format(f_x[ii],ii), end="")
+print()
+print("divided by")
+for ii in range(len(g_x)):
+    if ii == 0:      
+        print("({})".format(g_x[ii]), end="")    
+    elif g_x[ii] != 0:
+        print("+({})x^{}".format(g_x[ii],ii), end="")
+print()
+print("is")
+for ii in range(len(q)):
+    if ii == 0:      
+        print("[({:.4})".format(q[ii]), end="")    
+    elif q[ii] != 0:
+        print("+({:.4f})x^{}".format(q[ii],ii), end="")
+print("]",end ="")
+for ii in range(len(r)):
+    if ii == 0:
+        print("+[({:.4f})".format(r[ii]), end="")    
+    elif r[ii] != 0:
+        print("+({:.4f})x^{}".format(r[ii],ii), end="")
+print("]")        
+         
